accf2711eec6a4c9b46f01e34d088ace779f0c8c
nahuel61920 <estebancarrizo619@gmail.com>
2025-11-08 17:39:44 -0300

    refactor: :recycle: hash

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './Login'; // Importa tu componente de Login
 import Home from './Home';   // Otro componente, por ejemplo, una página principal
 import Usuarios from './Usuarios';
